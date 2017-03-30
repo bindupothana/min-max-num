a320b1cc75a6f4691ecccfd683eca1be5373114b
bindupothana <himabindu.pothana@navaratan.com>
2017-03-30 20:31:44 +0530

    Fifth

--- js/controller.js
+++ js/controller.js
@@ -9,38 +9,47 @@ app.controller('numCtrl',function($scope){
     $scope.enternum.push(num);
     console.log("print",angular.copy($scope.enternum));
     return localStorage.setItem('$scope.enternum', JSON.stringify($scope.enternum));
-    $scope.result=($scope.enternum)
+   
    }
 
 
   $scope.showResult=function(){
-  	$scope.result=[];
+    $scope.message="Maximum Number is:";
+    $scope.message="Minimum Number is:";
 
-	  	// $scope.min=0; 
+  	$scope.result=[];
+	  	$scope.min=0; 
 	  	 $scope.max=0;
-	  	//  $scope.sum=0;
+	  	
 // 	  		 $scope.min=$scope.enternum;
 // 	         $scope.max=$scope.enternum;
 // }          
-           for(i=0;i<=$scope.enternum;i++){
-           	// console.log("hhh",$scope.enternum)
-           	// $scope.avg = $scope.sum/$scope.enternum;
+           for(i=0;i<=$scope.enternum.length;i++){
 	           
 	           	if($scope.min>$scope.enternum[i]){
+                  console.log("hhh",$scope.min)
+
 	           		 $scope.min=angular.copy($scope.enternum[i]);
 	           		 console.log("kkkk",$scope.min)
 	           		}
 	           	else if($scope.max<$scope.enternum[i]){
+                                   console.log("hhh",$scope.max)
+
 
 		                $scope.max=angular.copy($scope.enternum[i]);
 		            }
+
+              
+
+
+
+
+
+
 	          	     console.log("hhh",$scope.max)
 
-	          		
-	          		
-
            }
-           $scope.avg = $scope.sum/$scope.enternum;
+           // $scope.avg = $scope.sum/$scope.enternum;
 
       }     
 
